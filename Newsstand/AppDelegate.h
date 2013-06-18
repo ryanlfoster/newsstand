f9b174b1f40a1469a339703998a6cf2447ac9066
Duan Nguyen <duannguyen@cnc.com.vn>
2013-06-18 11:03:52 +0700

    Initial Commit

---
+++ Newsstand/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Newsstand
+//
+//  Created by Nguyen Le Duan on 6/18/13.
+//  Copyright (c) 2013 Nguyen Le Duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Newsstand/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Newsstand
+//
+//  Created by Nguyen Le Duan on 6/18/13.
+//  Copyright (c) 2013 Nguyen Le Duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
